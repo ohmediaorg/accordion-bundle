ab33a95a06db378defb17109bcb6dcab01b2ca42
Justin Hoffman <justin@ohmedia.ca>
2024-04-17 15:40:40 -0600

    add fields to entities

--- src/Entity/Accordion.php
+++ src/Entity/Accordion.php
@@ -2,6 +2,8 @@
 
 namespace OHMedia\AccordionBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\AccordionBundle\Repository\AccordionRepository;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
@@ -16,6 +18,23 @@ class Accordion
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $faq = null;
+
+    /**
+     * @var Collection<int, AccordionItem>
+     */
+    #[ORM\OneToMany(targetEntity: AccordionItem::class, mappedBy: 'accordion', orphanRemoval: true)]
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
     public function __toString(): string
     {
         return 'Accordion #'.$this->id;
@@ -24,5 +43,59 @@ class Accordion
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isFaq(): ?bool
+    {
+        return $this->faq;
+    }
+
+    public function setFaq(bool $faq): static
+    {
+        $this->faq = $faq;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, AccordionItem>
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(AccordionItem $item): static
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setAccordion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(AccordionItem $item): static
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getAccordion() === $this) {
+                $item->setAccordion(null);
+            }
+        }
+
+        return $this;
     }
 }
--- src/Entity/AccordionItem.php
+++ src/Entity/AccordionItem.php
@@ -20,6 +20,16 @@ class AccordionItem
     #[ORM\Column(type: Types::SMALLINT)]
     private ?int $ordinal = 9999;
 
+    #[ORM\ManyToOne(inversedBy: 'items')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Accordion $accordion = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $header = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
     public function __toString(): string
     {
         return 'Accordion Item #'.$this->id;
@@ -38,6 +48,42 @@ class AccordionItem
     public function setOrdinal(int $ordinal): self
     {
         $this->ordinal = $ordinal;
+
+        return $this;
+    }
+
+    public function getAccordion(): ?Accordion
+    {
+        return $this->accordion;
+    }
+
+    public function setAccordion(?Accordion $accordion): static
+    {
+        $this->accordion = $accordion;
+
+        return $this;
+    }
+
+    public function getHeader(): ?string
+    {
+        return $this->header;
+    }
+
+    public function setHeader(string $header): static
+    {
+        $this->header = $header;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
 
         return $this;
     }
