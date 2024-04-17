3bec0dd9a964e0e4b65e63a75cb350ac256fa0db
Justin Hoffman <justin@ohmedia.ca>
2024-04-17 15:44:23 -0600

    set up the forms

--- src/Form/AccordionItemType.php
+++ src/Form/AccordionItemType.php
@@ -3,23 +3,9 @@
 namespace OHMedia\AccordionBundle\Form;
 
 use OHMedia\AccordionBundle\Entity\AccordionItem;
-// use Doctrine\ORM\EntityRepository;
-// use Doctrine\ORM\QueryBuilder;
-// use OHMedia\FileBundle\Form\Type\FileEntityType;
-// use OHMedia\MetaBundle\Form\Type\MetaEntityType;
-// use OHMedia\TimezoneBundle\Form\Type\DateTimeType;
-// use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use OHMedia\WysiwygBundle\Form\Type\WysiwygType;
 use Symfony\Component\Form\AbstractType;
-// use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-// use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-// use Symfony\Component\Form\Extension\Core\Type\EmailType;
-// use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-// use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-// use Symfony\Component\Form\Extension\Core\Type\NumberType;
-// use Symfony\Component\Form\Extension\Core\Type\TelType;
-// use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-// use Symfony\Component\Form\Extension\Core\Type\TextType;
-// use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -28,60 +14,15 @@ class AccordionItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $accordionItem = $options['data'];
+        $accordion = $accordionItem->getAccordion();
 
-        // TIP: these all do the same thing
-        // $builder->add('name');
-        // $builder->add('name', null);
-        // $builder->add('name', TextType::class);
+        $builder->add('header', TextType::class, [
+            'label' => $accordion->isFaq() ? 'Question' : 'Header',
+        ]);
 
-        // always use the file-bundle for files
-        // for some reason it is necessary to specify 'data'
-        // when usually 'mapped' => true (default) is enough
-        // $builder->add('file', FileEntityType::class, [
-        //     'data' => $accordionItem->getFile(),
-        // ]);
-        // $builder->add('image', FileEntityType::class, [
-        //     'image' => true,
-        //     'data' => $accordionItem->getFile(),
-        // ]);
-
-        // always use the datetime-bundle to ensure timezones are good
-        // $builder->add('start_datetime', DateTimeType::class);
-        // $builder->add('end_datetime', DateTimeType::class);
-
-        // if you have a checkbox for a toggle, make sure it is not required
-        // $builder->add('is_featured', CheckboxType::class, [
-        //     'required' => false,
-        // ]);
-
-        // <select>
-        // $builder->add('selection', ChoiceType::class);
-
-        // <select multiple>
-        // $builder->add('selection', ChoiceType::class, [
-        //     'multiple' => true,
-        // ]);
-
-        // array of <input type="radio">
-        // $builder->add('selection', ChoiceType::class, [
-        //     'expanded' => true,
-        // ]);
-
-        // array of <input type="checkbox">
-        // $builder->add('selection', ChoiceType::class, [
-        //     'expanded' => true,
-        //     'multiple' => true,
-        // ]);
-
-        // for a OneToOne or ManyToOne relationship selection
-        // $builder->add('owner', EntityType::class, [
-        //     'class' => User::class,
-        //     'query_builder' => function (EntityRepository $er): QueryBuilder {
-        //         return $er->createQueryBuilder('u')
-        //             ->orderBy('u.email', 'ASC');
-        //     },
-        //     'choice_label' => 'email',
-        // ]);
+        $builder->add('content', WysiwygType::class, [
+            'label' => $accordion->isFaq() ? 'Answer' : 'Content',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
