ef3b74376bbf295208e1a75c3c1775946ec7220b
Justin Hoffman <justin@ohmedia.ca>
2024-04-26 16:20:42 -0600

    update to new bundle directory structure

--- src/OHMediaAccordionBundle.php
+++ src/OHMediaAccordionBundle.php
@@ -2,8 +2,17 @@
 
 namespace OHMedia\AccordionBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class OHMediaAccordionBundle extends Bundle
+class OHMediaAccordionBundle extends AbstractBundle
 {
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $containerConfigurator,
+        ContainerBuilder $containerBuilder
+    ): void {
+        $containerConfigurator->import('../config/services.yaml');
+    }
 }
